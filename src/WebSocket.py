a876134a1391d85efcd5a4ded62d1d26af0f9e17
AngelVillanueva5 <sd.villanueva@student.avans.nl>
2024-04-03 10:59:22 +0200

    websocket is nu grotendeels functioneel, alleen nog integreren

--- src/WebSocket.py
+++ src/WebSocket.py
@@ -9,24 +9,25 @@ from websockets.server import serve
 async def echo(websocket):
     async for message in websocket:
 
-        toegangSnelheid = [60] 
-        ingangSnelheid = [50, 40, 30] 
-        centraleSnelheid = [50, 40, 30] 
-        verlatingSnelheid = [50, 40, 30] 
+        array = [60, 70, 50, 60]
+        data = autoPerZone(array)
 
-        snelheidAutoPerZone(toegangSnelheid, ingangSnelheid, centraleSnelheid, verlatingSnelheid)
-       
-        await websocket.send("test")
+        data = json.dumps(data)
+        data = str(data)
+        print(data)
+
+
+        await websocket.send(message)
 
 async def main():
     async with serve(echo, "localhost", 8765):
-        await asyncio.Future()  # run forever
+        await asyncio.Future()  # run forever 
 
 
 # 3B -> HMI
 
 # Actuele snelheid per auto per zone 
-async def snelheidAutoPerZone(toegangSnelheid, ingangSnelheid, centraleSnelheid, verlatingSnelheid):
+def snelheidAutoPerZone(toegangSnelheid, ingangSnelheid, centraleSnelheid, verlatingSnelheid):
         data = {}
         # naam van functie      
         data['functie'] = "snelheidAutoPerZone"
@@ -35,24 +36,21 @@ async def snelheidAutoPerZone(toegangSnelheid, ingangSnelheid, centraleSnelheid,
         data['snelHedeningang'] = ingangSnelheid
         data['snelHedencentrale'] = centraleSnelheid
         data['snelHedenverlating'] = verlatingSnelheid
-        json_data = json.dumps(data)
-      
-        print(data.getvalue())
-        return json_data
+
+        return data
 
 # Hoeveel auto’s per zone 
-async def autoPerZone(autos):
+def autoPerZone(autos):
         data = {}
         # naam van functie
         data['type'] = "autoPerZone"
         # doorsturen autos per zone | array index = zone [toegangszone, ingangszone, centralezone, verlatingzone] | array[15, 8, 9, 23]
         data['autos'] = autos
-        json_data = json.dumps(data)
 
-        print(json_data)
+        return data
 
 # SOS/storing bericht 
-async def sosBericht(statusSOS, storingBericht):
+def sosBericht(statusSOS, storingBericht):
         data = {}
         # naam van functie
         data['functie'] = "sosBericht"
@@ -60,30 +58,27 @@ async def sosBericht(statusSOS, storingBericht):
         data['statusSOS'] = statusSOS
         # Bericht voor de storing
         data['storingBericht'] = storingBericht
-        json_data = json.dumps(data)
 
-        print(json_data)
+        return data
 
 # Status bericht per LFV (storing) 
-async def lfvStatusStoring(storingLFV):
+def lfvStatusStoring(storingLFV):
         data = {}
         # naam van functie
         data['functie'] = "lfvStatusStoring"
         # array met storingen voor lfvs | array index = nummer LFV | array[false, true, false, false...]
         data['storingLFV'] = storingLFV
-        json_data = json.dumps(data)
 
-        print(json_data)
+        return data
 
 # Status bericht per LFV (status van LFV) 
-async def lfvStatussen(statusLFV):
+def lfvStatussen(statusLFV):
         data = {}
         # naam van functie
         data['functie'] = "lfvStatussen"
         # array met storing statussen voor lfvs | array index = nummer LFV | array[true, false, false, true...]
         data['statusLFV'] = statusLFV
-        json_data = json.dumps(data)
 
-        print(json_data)
+        return data
 
 asyncio.run(main())
